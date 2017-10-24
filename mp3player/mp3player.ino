b37e7da0781e09dbafad3a61d461ca0fde762988
caternuson <methuzla@speakeasy.net>
2017-10-24 12:35:59 -0700

    auto play next

--- mp3player/mp3player.ino
+++ mp3player/mp3player.ino
@@ -6,6 +6,7 @@
 #include <SD.h>
 #include <Adafruit_VS1053.h>
 
+// Guide is based on Feather M0 Express
 // ARDUINO_SAMD_FEATHER_M0 defines only
 // VS1053 Pins
 #define VS1053_RESET   -1     // VS1053 reset pin (not used!)
@@ -28,12 +29,19 @@
 #define VOL_MAX         50    // max volume (most quiet)
 // Maximum number of files (tracks) to load
 #define TRACKS_MAX      100
+// Player behavior
+#define AUTO_PLAY_NEXT  true  // true to automatically go to next track
 
 unsigned long currentMillis, previousMillis;
 int currentKnob, previousKnob;
 int volume;
 int currentTrack, totalTracks;
 char trackListing[TRACKS_MAX][13] = {' '};
+enum mode {
+  PLAYING,
+  PAUSED,
+  STOPPED
+} currentMode = STOPPED;
 
 Adafruit_VS1053_FilePlayer musicPlayer = 
   Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
@@ -41,7 +49,7 @@ Adafruit_VS1053_FilePlayer musicPlayer =
 //-----------------------------------------------------------------------------
 void setup() {
   Serial.begin(9600);
-  // Wait for serial port to be opened, remove this line for 'standalone' operation
+  // Leave commented for standalone operation, uncomment for troubleshooting
   //while (!Serial) ;
 
   // Initialize pins
@@ -62,6 +70,7 @@ void setup() {
      while (1);
   }
   Serial.println(F("VS1053 found"));
+  musicPlayer.softReset();
 
   // Make a tone to indicate VS1053 is working 
   musicPlayer.sineTest(0x44, 500);    
@@ -98,6 +107,18 @@ void loop() {
   // Update status LED
   updateStatusLED();
 
+  // Auto play next track if feature enabled
+  if (AUTO_PLAY_NEXT) {
+    if (currentMode==PLAYING && musicPlayer.stopped()) {
+      currentTrack = ++currentTrack < totalTracks ? currentTrack : 0; 
+      Serial.print("Next ");
+      Serial.print(currentTrack); Serial.print("=");
+      Serial.println(trackListing[currentTrack]);
+      musicPlayer.startPlayingFile(trackListing[currentTrack]);
+      currentMode = PLAYING;
+    }
+  }
+
   // Start / Stop
   if (!digitalRead(BUTTON_PLAY)) {
     if (musicPlayer.stopped()) {
@@ -105,9 +126,11 @@ void loop() {
       Serial.print(currentTrack); Serial.print("=");
       Serial.println(trackListing[currentTrack]);
       musicPlayer.startPlayingFile(trackListing[currentTrack]);
+      currentMode = PLAYING;
     } else {
       Serial.println("Stopped.");
       musicPlayer.stopPlaying();
+      currentMode = STOPPED;      
     }
     delay(250);
   }
@@ -117,10 +140,12 @@ void loop() {
     if (!musicPlayer.stopped()) {
       if (musicPlayer.paused()) {
         Serial.println("Resumed");
-        musicPlayer.pausePlaying(false);      
+        musicPlayer.pausePlaying(false);
+        currentMode = PLAYING;
       } else { 
         Serial.println("Paused");
         musicPlayer.pausePlaying(true);
+        currentMode = PAUSED;
       }    
     }
     delay(250);
@@ -129,6 +154,7 @@ void loop() {
   // Next
   if (!digitalRead(BUTTON_NEXT)) {
     if (!musicPlayer.stopped()) {
+      Serial.println("Stopping current playback.");
       musicPlayer.stopPlaying();
     }
     currentTrack = ++currentTrack < totalTracks ? currentTrack : 0; 
@@ -136,13 +162,16 @@ void loop() {
     Serial.print(currentTrack); Serial.print("=");
     Serial.println(trackListing[currentTrack]);
     musicPlayer.startPlayingFile(trackListing[currentTrack]);
+    currentMode = PLAYING;
     delay(250);
   }
 }
 
 //-----------------------------------------------------------------------------
 void updateVolume() {
+  // Read current value
   currentKnob = analogRead(KNOB_VOLUME);
+  // Only update if it's changed
   if (currentKnob != previousKnob) {
     previousKnob = currentKnob;
     volume = map(currentKnob, KNOB_MIN, KNOB_MAX, VOL_MIN, VOL_MAX);
@@ -153,17 +182,17 @@ void updateVolume() {
 //-----------------------------------------------------------------------------
 void updateStatusLED() {
   if (musicPlayer.paused()) {
-    // blink it like a polaroid
+    // Blink it like a polaroid
     currentMillis = millis();
     if (currentMillis - previousMillis > BLINK_RATE) {
        previousMillis = currentMillis;
        digitalWrite(LED_STATUS, !digitalRead(LED_STATUS));
     }
   } else if (!musicPlayer.stopped()) {
-    // it's so on again
+    // It's so on again
     digitalWrite(LED_STATUS, HIGH);
   } else {
-    // it's so off again
+    // It's so off again
     digitalWrite(LED_STATUS, LOW);
   }
 }
@@ -174,12 +203,12 @@ void loadTracks(File dir, int level) {
     File entry = dir.openNextFile();
     if (!entry) return;
     if (entry.isDirectory()) {
-      // recursive call to scan next dir level
+      // Recursive call to scan next dir level
       loadTracks(entry, level + 1);
     } else {
-      // only add files in root dir
+      // Only add files in root dir
       if (level == 0) {
-        // and only if they have good names
+        // And only if they have good names
         if (nameCheck(entry.name())) {
           strncpy(trackListing[totalTracks], entry.name(), 12);
           Serial.print(totalTracks); Serial.print("=");
@@ -189,7 +218,7 @@ void loadTracks(File dir, int level) {
       }
     }
     entry.close();
-    // stop scanning if we hit max
+    // Stop scanning if we hit max
     if (totalTracks >= TRACKS_MAX) return;
   } 
 }
@@ -197,11 +226,15 @@ void loadTracks(File dir, int level) {
 //-----------------------------------------------------------------------------
 bool nameCheck(char* name) {
   int len = strlen(name);
-  // check length
+  // Check length
   if (len <= 4) return false;
-  // check extension    
-  if (!(strncmp(name + len - 3, "MP3", 3)==0)) return false;
-  // check first character
+  // Check extension
+  char* ext = strrchr(name,'.');
+  if (!(
+    strcmp(ext,".MP3") == 0  ||
+    strcmp(ext,".OGG") == 0
+    )) return false;
+  // Check first character
   switch(name[0]) {
     case '_': return false;
   }
